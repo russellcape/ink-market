75bc922bc6361c352c4cce28148d8a0db38a55d8
Russell <russellcape98@gmail.com>
2020-05-21 17:45:01 -0400

    Added a banner and inputs in header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        
+    <div>
+      <header className="Banner">
+        <h1 className="BannerTitle">Ink Market</h1>
+        <div className="BannerForms">
+        <form className="Location">
+          <input type="text" placeholder="My Location"/>
+            <button>
+              Find Me
+            </button>
+          </form>
+          <br/>
+          <form className="Search">
+            <input type="text" placeholder="Style, Shop, Artist"/>
+            <button>
+              Search
+            </button>
+          </form>
+        </div>
       </header>
     </div>
   );
